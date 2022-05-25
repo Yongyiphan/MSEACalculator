9d1eb12400ce9c322d25df26c4f59429b917e020
Yongyiphan <edgar.yong.yip.han@gmail.com>
2022-05-25 20:00:37 +0800

    In Progress: Event Tracker

--- EventRes/EventRecordsCLS.cs
+++ EventRes/EventRecordsCLS.cs
@@ -17,20 +17,6 @@ namespace MSEACalculator.EventRes
         public Dictionary<string, bool> availableDays { get; set; }
         public List<Dictionary<string, int>> chartrackCoins { get; set; }
 
-
-
         public EventRecordsCLS() { }
-
-        public EventRecordsCLS(string eventTitle, DateTime start, DateTime end, int duration,
-            Dictionary<string, bool> availableDays)
-        {
-            this.eventTitle = eventTitle;
-            this.startDate = start;
-            this.endDate = end;
-            this.eventDuration = duration;
-            this.availableDays = availableDays;
-            //this.chartrackCoins = characterCoins;
-
-        }
     }
 }
